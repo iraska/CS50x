d7a764a473da8a18512120d0f3275ac23c46a5e1
İrem Turna <105501017+iraska@users.noreply.github.com>
2023-05-30 17:20:18 +0300

    Some helpful comments added.

--- HelloTask.c
+++ HelloTask.c
@@ -1,3 +1,6 @@
+// Getting Input from User
+// Modify this program in such a way that it prompts the user to enter their name first (prompt). Then let it print hello, dunk-who, - here i-know-who will be the user's real name.
+
 #include <stdio.h>
 #include <cs50.h>
 
